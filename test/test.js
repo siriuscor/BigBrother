73774c95541d07826a8b95bfdbf58fa9b8a508dc
Li Yi <siriuscor@gmail.com>
2019-05-08 18:34:35 +0800

    add log error test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,71 @@
-// function tt() {
-//   var args = Array.prototype.slice.call(arguments);
-//   console.log(args.map((i)=> {return JSON.stringify(i);}).join(' '));
-// }
+const winston = require('winston');
+var spawn = require('child_process').spawn;
+// var logger, proc;
+var running = {};
 
-// tt(1,2,3,4, {a:1});
-console.log(process.env);
-let i = 0;
+var id = 0;
+function start() {
+  var new_logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => {
+            return `${info.timestamp} ${info.level}: ${info.message.toString().replace(/\n$/, '')}`;
+        })
+    )
+  });
+  new_logger.id = id++;
+  console.log('new logger with id', new_logger.id);
+  
+  var fileTrans = new winston.transports.File({
+    level: 'info',
+    tailable: true,
+    maxsize: 10 * 1024,
+    maxFiles: 2,
+    filename: 'test.log'
+  });
+  fileTrans._stream.id = new_logger.id;
+  fileTrans._stream.on('error',  ()=>{});
+  // console.log('listen error event', new_logger.id);
+  new_logger.add(fileTrans);
+  new_logger.on('error', ()=>{});
+
+  let old_logger = running['logger'];
+  // if (running['logger']) running['logger'].close();
+  running['logger'] = new_logger;
+  // console.log('replace logger with ', new_logger.id);
+
+  // if (old_logger) {
+  //   old_logger.close();
+  //   console.log('close old logger', old_logger.id);
+  // }
+  let proc = spawn('yes', [], {stdio: ['ignore', 'pipe', 'pipe']});
+  proc.stdout.on('data', () => {
+    // console.log('logger with ', running['logger'].id);
+    for(let i = 0; i < 10; i ++)
+    running['logger'].info('keep logging long message' + '~'.repeat(1000));
+  });
+  running['proc'] = proc;
+}
 setInterval(() => {
-  console.log(`log ${i} ${new Date()}`);
-  console.error(`error ${i} ${new Date()}`);
-}, 1000);
+  if (running['proc']) running['proc'].kill();
+  start();
+  console.log(process.memoryUsage()['heapUsed']);
+}, 100);
+
+process.on('uncaughtException', (e) => {
+  console.error('uncaught exception', e.stack);
+  require('fs').appendFileSync('test-err.log', e.stack ? e.stack: JSON.stringify(e));
+  process.exit(1);
+});
+
+
+  // fileTrans._stream.on('error', (e) => {
+  //   console.log(e);
+  // });
+// fileTrans.on('closed', () => {
+//   console.log('closed');
+//   logger.info('after closed');
+// });
+
+
+
